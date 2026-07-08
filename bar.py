97f608bc6c943328c70de812fa5cec2c944971df
LuminTu2 <lumintu@126.com>
2026-07-08 09:06:43 +0800

    type-bar

---
+++ bar.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""=================================================
+# @Time : 2026-07-08 09:06:21
+# @IDE -> Interpreter: PyCharm 2021.3 -> python 3.8.10
+# @Path : ./useJupyter//bar.py
+# @Author : Lenovo
+# @Describe : 
+================================================="""
+
+if __name__ == '__main__':
+    print("bar-type")
