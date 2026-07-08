4b50dc8d44b902316133ffd706c9daa3566be09e
LuminTu2 <lumintu@126.com>
2026-07-08 09:26:31 +0800

    .coca to .bar

---
+++ coca.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""=================================================
+# @Time : 2026-07-08 09:25:48
+# @IDE -> Interpreter: PyCharm 2021.3 -> python 3.8.10
+# @Path : ./useJupyter//coca.py
+# @Author : Lenovo
+# @Describe : 
+================================================="""
+if __name__ == '__main__':
+    print("coca")
