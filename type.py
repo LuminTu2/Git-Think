c08bc141cd5bc659046c68d1134ae4d5fbd2ea04
LuminTu2 <lumintu@126.com>
2026-07-08 09:26:31 +0800

    .coca

---
+++ type.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""=================================================
+# @Time : 2026-07-08 09:26:08
+# @IDE -> Interpreter: PyCharm 2021.3 -> python 3.8.10
+# @Path : ./useJupyter//type.py
+# @Author : Lenovo
+# @Describe : 
+================================================="""
+if __name__ == '__main__':
+    print("type-coca")
